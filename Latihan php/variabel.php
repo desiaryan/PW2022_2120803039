44c284aa55c7d7b05a16eae4858b0d856c1adc2c
desiaryan <desiar244@gmail.com>
2022-11-02 01:56:23 +0700

    Tugas P5

---
+++ Latihan php/variabel.php
@@ -0,0 +1,20 @@
+<?<?php 
+
+$a = "Hello";
+$hello = "Hello World"
+
+// menampilkan isi variabel $a
+// hello
+echo $a . "</br>";
+
+// menampillkan isi variavel $a
+// Hello World!
+echo $hello . "</br>";
+
+// menampilkan isi dari variabel dengan nama yang sama seperti isi variabel $a
+// isi variabel $a = hello. Jadi, nanti akan menampilkan isi dari variabel 
+$hello
+// Hello World
+echo $$a . "</br>";
+
+?>
